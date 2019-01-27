6d980e4e4963e4e6d7f240956e9182dfd9476f31
Ahmed Ramzi <ahmedramzy160@yahoo.com>
2019-01-27 23:12:32 +0800

    Updates

---
+++ precache-manifest.066fd3d56f92bdec0d8eb080a2fff55b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "43006f0adf067573e343",
+    "url": "/react-bidboard/static/css/main.3843a717.chunk.css"
+  },
+  {
+    "revision": "43006f0adf067573e343",
+    "url": "/react-bidboard/static/js/main.43006f0a.chunk.js"
+  },
+  {
+    "revision": "0a3e27a9720b70813938",
+    "url": "/react-bidboard/static/css/1.f9e4c855.chunk.css"
+  },
+  {
+    "revision": "0a3e27a9720b70813938",
+    "url": "/react-bidboard/static/js/1.0a3e27a9.chunk.js"
+  },
+  {
+    "revision": "546fdd7277c853029483",
+    "url": "/react-bidboard/static/js/runtime~main.546fdd72.js"
+  },
+  {
+    "revision": "fef1f20aa76540808c4f5b004bfe49bd",
+    "url": "/react-bidboard/static/media/loader.fef1f20a.gif"
+  },
+  {
+    "revision": "581c6325b471e37cc340dddbcc3975bb",
+    "url": "/react-bidboard/index.html"
+  }
+];
